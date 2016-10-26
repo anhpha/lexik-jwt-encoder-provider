0d7fb4aa68f10260154faf115e86c75b4e8ac375
Anh Pha <phava@minhhungland.vn>
2016-10-26 16:11:53 +0700

    Create a shared repo for lexik encoder provider

---
+++ Mhl/Lexik/JWTEncoder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Mhl\Lexik;
+
+use Mhl\Lexik\OpenSSLKeyLoader;
+use Namshi\JOSE\SimpleJWS;
+use Symfony\Component\Validator\Exception\InvalidArgumentException;
+
+/**
+ * JWTEncoder
+ *
+ * @author Dev Lexik <dev@lexik.fr>
+ */
+class JWTEncoder implements JWTEncoderInterface
+{
+    const ALGORYTHM = 'RS256';
+
+    /**
+     * @var OpenSSLKeyLoader
+     */
+    protected $keyLoader;
+
+    /**
+     * @param OpenSSLKeyLoader $keyLoader
+     */
+    public function __construct(OpenSSLKeyLoader $keyLoader)
+    {
+        $this->keyLoader = $keyLoader;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function encode(array $data)
+    {
+        $jws = new SimpleJWS(['alg' => self::ALGORYTHM]);
+        $jws->setPayload($data);
+        $jws->sign($this->keyLoader->loadKey('private'));
+
+        return $jws->getTokenString();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function decode($token)
+    {
+        try {
+            /** @var SimpleJWS $jws */
+            $jws = SimpleJWS::load($token);
+        } catch (InvalidArgumentException $e) {
+            return false;
+        }
+
+        if (!$jws->isValid($this->keyLoader->loadKey('public'), self::ALGORYTHM)) {
+            return false;
+        }
+
+        return $jws->getPayload();
+    }
+}
---
+++ Mhl/Lexik/JWTEncoderInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mhl\Lexik;
+
+/**
+ * JWTEncoderInterface
+ *
+ * @author Nicolas Cabot <n.cabot@lexik.fr>
+ */
+interface JWTEncoderInterface
+{
+    /**
+     * @param array $data
+     *
+     * @return string the encoded token string
+     */
+    public function encode(array $data);
+
+    /**
+     * @param string $token
+     *
+     * @return bool|array
+     */
+    public function decode($token);
+}
